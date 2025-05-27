de6b4cb206379d1de9bc228d39dcc8f7fc231178
Nicolas Campos <nicolascampos611@gmail.com>
2025-05-26 23:26:43 -0300

    Adicionado mais cores e ao chegar no final da lista de cores, ele volta para o início e reinicia

--- src/app/components/change-color/change-color.component.ts
+++ src/app/components/change-color/change-color.component.ts
@@ -9,8 +9,13 @@ import { Component } from '@angular/core';
 export class ChangeColorComponent {
   isActive = true;
 
-  changeBackground() {
-    document.body.style.backgroundColor = "red";
+  colors = ['red', 'green', 'yellow', 'blue', 'pink', 'ourole', 'gray']
+
+  position = 0;
+
+  changeBackground() { 
+    document.body.style.backgroundColor = this.colors[this.position]; //Define a cor de fundo da página usando a cor atual do array
+    this.position = (this.position + 1) % this.colors.length; //Avança para a próxima posição no array de cores, se chegar ao fim, volta para a primeira cor automaticamente
   }
 
 }
